fddb73b15a36569fa3a5bc8bccc03da455c46d32
Andreysambar <andreysambar@gmail.com>
2024-08-28 23:22:00 +0300

    feat: prepare jquery plugin environment

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world!");
+import $ from "jquery";
+
+(function($){
+  jQuery.fn.coolSlider3000 = function(options) {
+
+    let make = function (){
+      //code
+    }
+
+    return this.each(make);
+  };
+})(jQuery);
